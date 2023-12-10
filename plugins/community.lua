a41ec5fb36eabcaaa862dad7809506fe25cc6b4f
Fadi Asaad <firon1222@gmail.com>
2023-12-10 04:08:14 +0200

    move most of plugins to community editions to make config files less ugly.

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,8 +5,38 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.pack.dart" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.completion.tabnine-nvim" },
   { import = "astrocommunity.media.vim-wakatime" },
+
   { import = "astrocommunity.motion.hop-nvim" },
+
   { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.color.twilight-nvim" },
+
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+
+  { import = "astrocommunity.git.neogit" },
+
+  { import = "astrocommunity.editing-support.chatgpt-nvim" },
+  { import = "astrocommunity.editing-support.nvim-devdocs" },
+  { import = "astrocommunity.editing-support.vim-move" },
+  { import = "astrocommunity.editing-support.neogen" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+
+  { import = "astrocommunity.register.nvim-neoclip-lua" },
+
+  { import = "astrocommunity.code-runner.compiler-nvim" },
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+  { import = "astrocommunity.code-runner.sniprun" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -2,69 +2,15 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  -- {
-  --   "tzachar/cmp-tabnine",
-  --   config = true,
-  --   opts = {
-  --     show_prediction_strength = true,
-  --   },
-  --   build = "./install.sh",
-  -- },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = {
-      sources = {
-        { name = "nvim_lsp" },
-        -- { name = "cmp_tabnine" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "nvim_lua" },
-        { name = "path" },
-      },
-      formatting = {
-        format = require("lspkind").cmp_format {
-          mode = "symbol",
-          before = function(_, vim_item)
-            vim_item.menu = " [" .. vim_item.kind .. "] "
-            return vim_item
-          end,
-        },
-      },
-    },
-    dependencies = {
-      -- { "tzachar/cmp-tabnine" },
-      { "onsails/lspkind.nvim" },
-    },
-  },
-  {
-    "weilbith/nvim-code-action-menu",
-    cmd = { "CodeActionMenu" },
-  },
-  {
-    "kosayoda/nvim-lightbulb",
-    event = { "User AstroFile" },
-    opts = {
-      autocmd = { enabled = true },
-      status_text = { enabled = true },
-    },
-    dependencies = { "antoinemadec/FixCursorHold.nvim" },
-  },
   {
     "mg979/vim-visual-multi",
-    config = true,
-    keys = { "<C-n>" },
+    event = "User AstroFile",
+    opt = {},
   },
   {
     "Exafunction/codeium.vim",
-    -- event = "User AstroFile",
-    event = "BufEnter",
+    event = "User AstroFile",
+    -- event = "BufEnter",
     config = function()
       -- disable default mappings
       vim.g.codeium_disable_bindings = 1
@@ -110,53 +56,34 @@ return {
       end, { noremap = true, desc = "Toggle Codeium active" })
     end,
   },
-  {
-    "folke/todo-comments.nvim",
-    config = true,
-    event = "User AstroFile",
-    cmd = { "TodoTrouble", "TodoTelescope", "TodoQuickFix", "TodoLocList" },
-  },
-  {
-    "akinsho/flutter-tools.nvim",
-    opts = {
-      statusline = {
-        app_version = true,
-        device = true,
-        project_config = true,
-      },
-      debugger = {
-        enabled = true,
-        run_via_dap = true,
-      },
-      fvm = true,
-      lsp = {
-        color = {
-          enabled = true,
-        },
-      },
-    },
-  },
-  {
-    "Nash0x7E2/awesome-flutter-snippets",
-    ft = "dart",
-    config = function() require("luasnip.loaders.from_vscode").lazy_load { include = { "dart" } } end,
-  },
+  -- {
+  --   "akinsho/flutter-tools.nvim",
+  --   opts = {
+  --     statusline = {
+  --       app_version = true,
+  --       device = true,
+  --       project_config = true,
+  --     },
+  --     debugger = {
+  --       enabled = true,
+  --       run_via_dap = true,
+  --     },
+  --     fvm = true,
+  --     lsp = {
+  --       color = {
+  --         enabled = true,
+  --       },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "Nash0x7E2/awesome-flutter-snippets",
+  --   ft = "dart",
+  --   config = function() require("luasnip.loaders.from_vscode").lazy_load { include = { "dart" } } end,
+  -- },
   {
     "segeljakt/vim-silicon",
     config = true,
     cmd = { "Silicon" },
-  },
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = true,
-    -- config = function()
-    --   require("chatgpt").setup()
-    -- end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
   },
 }
